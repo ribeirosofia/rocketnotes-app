d488da1e1a55a12e90837c8ceae7f4131e17e322
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-04-18 16:29:44 -0300

    feat: create link section

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Container } from './styles'
-import Header  from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Container} from './styles'
+import Header  from '../../components/Header'
+import { Links } from '../../components/LinkSection/styles'
+import { LinkSection } from '../../components/LinkSection'
+
 
  const Details: React.FC<{}> = () => {
   return(
     <>
     <Container>
-      <Header></Header>
+      <Header />
+      <LinkSection title="Links úteis">
+        <Links>
+        <li>
+          <a href="#">Link 1</a>
+        </li>
+        </Links>
+      </LinkSection>
       <Button title="Voltar"/>
     </Container>
     </>
