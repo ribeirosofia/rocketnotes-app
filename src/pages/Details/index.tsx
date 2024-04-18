805c4ca88639d5d5b23de2a2fd3414d14dabeca1
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-04-18 18:34:40 -0300

    feat: style details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '../../components/Button'
 import { ButtonText } from  '../../components/ButtonText'
-import { Container} from './styles'
+import { Container, Content} from './styles'
 import Header  from '../../components/Header'
 import { Links } from '../../components/LinkSection/styles'
 import { LinkSection } from '../../components/LinkSection'
@@ -12,19 +12,36 @@ import { Tag } from '../../components/Tag'
     <>
     <Container>
       <Header />
-      <ButtonText title="Excluir nota"/>
-      <LinkSection title="Links úteis">
-        <Links>
-        <li>
-          <a href="#">Link 1</a>
-        </li>
-        </Links>
-      </LinkSection>
-      <LinkSection title="Marcadores">
-       <Tag title="Express"/>
-       <Tag title="Node.js"/>
-      </LinkSection>
-      <Button title="Voltar" loading />
+            <main>
+              <Content>
+                <ButtonText title="Excluir nota"/>
+                <h1>
+                  Introdução ao React
+                </h1>
+
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+                <LinkSection title="Links úteis">
+                <Links>
+                <li>
+                  <a href="#">Link 1</a>
+                </li>
+                </Links>
+                <Links>
+                <li>
+                  <a href="#">Link 2</a>
+                </li>
+                </Links>
+                </LinkSection>
+                <LinkSection title="Marcadores">
+                <Tag title="Express"/>
+                <Tag title="Node.js"/>
+                </LinkSection>
+                <Button title="Voltar" loading />
+              </Content>  
+            </main>      
+            
     </Container>
     </>
   )
