e0b0990bf34857527aea5dbd43ddaccc6dead506
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-04-24 02:56:47 -0300

    refactor: edit note component

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -9,7 +9,11 @@ export const Note = ({data, ...rest}) =>{
         {
             data.tags &&
             <footer>
-                {data.tags.map(tag =>  <Tag key={tag.id} title={tag.name}/>
+                {data.tags.map(tag =>  
+                <Tag 
+                key={tag.id} 
+                title={tag.name}
+                />
                 )}
             </footer>
         }
