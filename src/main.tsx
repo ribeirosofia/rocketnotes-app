d48ae49edec5990b0cbb6557643eed5b4787cc4e
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-04-24 04:29:05 -0300

    feat: create sing in page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home  from './pages/Home'
 import GlobalStyles from './styles/global'
+import SingIn  from './pages/SingIn'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
