2a1acc21076e3ab49770f9a0e635f8a9c51add0a
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-04-25 00:07:56 -0300

    feat: finish page new

--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -3,7 +3,8 @@ import { Button } from '../../components/Button'
 import { Container, Form } from './styles'
 import  Header  from '../../components/Header'
 import { Input } from '../../components/Input';
-import  Tag  from '../../components/Tag'
+import { LinkSection } from '../../components/LinkSection'
+import  NoteItem  from '../../components/NoteItem/index.jsx';
 import { Textarea } from '../../components/Textarea/index.jsx';
 
 const New: React.FC<{}> = () => {
@@ -19,7 +20,25 @@ const New: React.FC<{}> = () => {
                         </header>
                         <Input placeholder="Título" type="text" icon={""}/>
                         <Textarea placeholder="Observações" value=""/>
-
+                        <LinkSection title="Links úteis">
+                            <NoteItem 
+                            value="https://www.w3schools.com/" 
+                            />
+                            <NoteItem 
+                            isNew placeholder="Novo link"
+                            />
+                        </LinkSection>
+                        <LinkSection title="Marcadores">
+                            <div className='tags'>
+                                <NoteItem 
+                                value="React.js" 
+                                />
+                                <NoteItem 
+                                isNew placeholder="Nova Tag"
+                                />
+                            </div>
+                        </LinkSection>
+                            <Button title="Salvar" loading={false}/>
                     </Form>
                 </main>
             </Container>
